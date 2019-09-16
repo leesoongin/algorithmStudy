c72af074dde8a38401196a9e902b77a1834f2098
leesoongin <tnddls2ek@naver.com>
2019-09-16 17:34:07 +0900

    StarPrint_3

---
+++ src/StarPrint_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StarPrint_3 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int line;
+
+        System.out.println("몇줄 만드실?");
+        line=input.nextInt();
+
+        for(int i=line-1;i>=0;i--) {
+            for(int j=0;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
