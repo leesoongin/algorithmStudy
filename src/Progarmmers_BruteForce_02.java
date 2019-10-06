f046e46856fab545f537383413c5b25c32804c8f
leesoongin <tnddls2ek@naver.com>
2019-10-06 23:05:27 +0900

    Programmers_BruteForce_02

--- src/Progarmmers_BruteForce_02.java
+++ src/Progarmmers_BruteForce_02.java
@@ -1,38 +1,112 @@
 import java.util.ArrayList;
 
 public class Progarmmers_BruteForce_02 {
+    static ArrayList<String> test=new ArrayList<String>();
+    static ArrayList<Integer> testParseInt=new ArrayList<Integer>();
 
-    ArrayList<String> combinationString = new ArrayList<String>();
+    static ArrayList<Integer> permitionNumber=new ArrayList<Integer>();
 
-    public static void main(String[] args) {
-        String numbers = "17";
+    public static int solution(String numbers){
 
-        solution(numbers);
-    }
+        String[] arr=new String[numbers.length()];
+        int numSize=10;
+        int answer=0;
+        boolean isCheck=false;
 
-    public static int solution(String numbers) {
-        int answer = 0;
-
-        int[] numArr = new int[numbers.length()];
-
-        for (int i = 0; i < numArr.length; i++) {
-            numArr[i] = Integer.parseInt("" + numbers.charAt(i));
+        for(int i=0;i<arr.length;i++) {
+            arr[i]=""+numbers.charAt(i);
         }
 
-        dfs(numArr,0,"");
+        /** 길이에 따라 소수의 크기 찾기위해서 **/
+        if(arr.length > 1) {//길이가 1보다 크다면
+            for(int i=1;i<arr.length;i++) {
+                numSize=numSize*10;
+            }
+        }
 
+        int[] perNum=new int[numSize+1]; //소수를 찾을 범위만큼의 배열
+
+        for(int i=2;i<perNum.length;i++) { //초기화
+            perNum[i]=i;
+        }
+
+        for(int i=2;i<perNum.length;i++) {//찾고싶은 범위만큼의 소수를 찾기
+            if(perNum[i] == 0)
+                continue;
+            for(int j=i+i;j<perNum.length;j+=i) {
+                perNum[j]=0;
+            }
+        }
+
+        for(int i=0;i<perNum.length;i++) { //찾은 소수를 arraylist에 저장 이걸로 비교할거임
+            if(perNum[i] != 0)
+                permitionNumber.add(perNum[i]);
+        }
+
+        for(int i=0;i<=arr.length;i++) {
+            perm(arr,0,arr.length,i);
+        }
+
+        for(int i=0;i<test.size();i++) { //문자열로 저장되어있는걸 정수형으로 전환 . 밑에서 소수인지 판별할때 편하게 하기위해서.
+            if(test.get(i) != "") {
+                for(int j=0;j<testParseInt.size();j++) {
+                    if(Integer.parseInt(test.get(i)) == testParseInt.get(j)) {
+                        //System.out.println(Integer.parseInt(test.get(i)));
+                        isCheck=true;//중복이 존재한다.
+                        break;
+                    }
+                }//inner for
+                if(!isCheck) { //중복이 존재하지 않는다면.
+                    testParseInt.add(Integer.parseInt(test.get(i)));
+                }//inner if
+                isCheck=false;
+            }//if
+        }
+
+
+        for(int i=0;i<permitionNumber.size();i++) {
+            for (int j = 0; j < testParseInt.size(); j++) {
+                if (permitionNumber.get(i) == testParseInt.get(j)) {
+                    answer++;
+                    //System.out.println(permitionNumber.get(i));
+                }
+
+            }
+        }
         return answer;
     }
 
-    public static void findPerMutaion(int[] numArr, int index) {
-        //전체 순회하면서 노드 만들고 노드를 하나씩 순회할때마다 검사합수 돌려서 해볼까?
+    public static void main(String[] args) {
+        String numbers="0000101";
+        int result;
+
+        result=solution(numbers);
+
+        System.out.println(result);
     }
 
-    public static void dfs(int[] numArr, int index, String combination) {
-            if(numArr.length == combination.length()){ //길이가 같아지면 return
-                return;
+    public static void perm(String[] arr, int depth, int n, int k) {
+
+        if (depth == k) { // 한번 depth 가 k로 도달하면 사이클이 돌았음. 출력함.
+            String str="";
+            for(int i=0;i<k;i++) {
+                str+=arr[i];
             }
+            test.add(str);
+            return;
+        }
 
+        for (int i = depth; i< n; i++) {
+            swap(arr, i, depth);
+            perm(arr, depth + 1, n, k);
+            swap(arr, i, depth);
+        }
 
-    }//dfs
+    }
+
+    public static void swap(String[] arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
