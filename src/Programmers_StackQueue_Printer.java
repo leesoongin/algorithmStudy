dd915c7f676b0f499ecbc4c07e2df157ab504107
leesoongin <tnddls2ek@naver.com>
2019-11-21 02:42:53 +0900

    프로그래머스 프린터

---
+++ src/Programmers_StackQueue_Printer.java
@@ -0,0 +1,32 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Programmers_StackQueue_Printer {
+    public static void main(String[] args){
+        int[] priorities={1,1,9,1,1,1};
+        int location=0;
+
+        System.out.println(solution(priorities,location));
+    }
+
+    public static int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> queue=new PriorityQueue<>(Collections.reverseOrder());
+        int answer=0;
+
+        for(int i=0;i<priorities.length;i++){
+           queue.offer(priorities[i]);
+        }
+
+       while(!queue.isEmpty()){
+           for(int i=0;i<priorities.length;i++){
+               if(queue.peek() == priorities[i]){
+                   if(i == location){ return answer+1;}
+                   queue.poll();
+                   answer++;
+               }//if
+           }//for
+       }//while
+
+        return answer+1;
+    }
+}
